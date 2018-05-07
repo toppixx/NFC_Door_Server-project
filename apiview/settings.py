2acca8ffc30db44af9cc788d7d2e2a2086aa7258
ubuntu <TobiasFaller@gmx.de>
2018-05-07 14:00:30 -0700

    added psycopg2-binary to requirements.txt

--- apiview/settings.py
+++ apiview/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'apiview.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'myproject',
+        'USER': 'myprojectuser',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -121,7 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_URL = '/static/'
-
 
 AUTH_USER_MODEL = "profiles_api.UserProfile"
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
