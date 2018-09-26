30b83408316fe67b1f6b5e92b668bfa5121ebdff
ubuntu <TobiasFaller@gmx.de>
2018-09-26 01:07:19 -0700

    changed url. All NFC could now be found under /nfc/*

--- doorAccess/urls.py
+++ doorAccess/urls.py
@@ -10,9 +10,9 @@ router.register('profile', doorAccews_views.UserProfileViewSet)
 router.register('login', doorAccews_views.LoginViewSet, base_name='login')
 router.register('feed', doorAccews_views.UserProfileFeedViewSet)
 router.register('access', doorAccews_views.DoorAccesControllViewSet)
-router.register('NFCDoorAcContPhase1', doorAccews_views.NfcDooorAcContPhase1ViewSet, base_name='NfcDACP1')
-router.register('NFCDoorAcContPhase2', doorAccews_views.NfcDooorAcContPhase2ViewSet, base_name='NfcDACP2')
-router.register('NFCDoorAcContPhase3', doorAccews_views.NfcDooorAcContPhase3ViewSet, base_name='NfcDACP3')
+router.register('DoorAcContPhase1', doorAccews_views.NfcDooorAcContPhase1ViewSet, base_name='NfcDACP1')
+router.register('DoorAcContPhase2', doorAccews_views.NfcDooorAcContPhase2ViewSet, base_name='NfcDACP2')
+router.register('DoorAcContPhase3', doorAccews_views.NfcDooorAcContPhase3ViewSet, base_name='NfcDACP3')
 urlpatterns = [
     path('hello-view', doorAccews_views.HelloApiView.as_view()),
     path('',include(router.urls))
