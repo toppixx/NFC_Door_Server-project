ec90719225cc2a0e0220c48e1927e055f8b11471
Unknown <TobiasFaller@gmx.de>
2018-10-08 00:45:40 -0700

    asdfghjk

--- doorAccess/models.py
+++ doorAccess/models.py
@@ -306,7 +306,7 @@ class NfcListOfUsers(models.Model):
 
 
                             print("\n")
-                            cipherText = cipherText.hex().uppper()
+                            cipherText = cipherText.hex().upper()
                             print(cipherText)
                             sha256Hash = hashlib.sha256(cipherText.hex().encode('ascii'))
                             # print("sha256Hash")
